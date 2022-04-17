47f5679439e0cd0d54cf7fe112105c8d7eccabc1
Martin Traverse <martin.traverse@gmail.com>
2022-04-17 10:17:46 +0100

    ColumnChunkMetadata, without encryption or compression support.

---
+++ java/adapter/parquet/src/main/java/org/apache/arrow/adapter/parquet/ColumnChunkMetadata.java
@@ -0,0 +1,214 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.arrow.adapter.parquet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.arrow.adapter.parquet.type.ParquetType;
+import org.apache.arrow.adapter.parquet.type.SortOrder;
+import org.apache.parquet.format.ColumnChunk;
+import org.apache.parquet.format.ColumnCryptoMetaData;
+import org.apache.parquet.format.ColumnMetaData;
+
+
+/** ColumnChunkMetaData is a proxy around Thrift format ColumnChunkMetaData. */
+public class ColumnChunkMetadata {
+
+  private final List<Encoding> encodings;
+  private final List<PageEncodingStats> encodingStats;
+  private final ColumnChunk column;
+  private final ColumnMetaData columnMetaData;
+  private final ColumnDescriptor descriptor;
+  private final ApplicationVersion writerVersion;
+
+  private Statistics possibleStats;
+
+  // Only used during initialization when metadata is encrypted
+  // private final ColumnMetaData decryptedMetadata;
+
+  /** ColumnChunkMetaData is a proxy around Thrift format ColumnChunkMetaData. */
+  public ColumnChunkMetadata(
+      ColumnChunk column,
+      ColumnDescriptor descr,
+      short rowGroupOrdinal, short columnOrdinal,
+      ApplicationVersion writerVersion
+      /*, InternalFileDecryptor file_decryptor */ ) {
+
+    this.column = column;
+    this.descriptor = descr;
+    this.writerVersion = writerVersion;
+
+    this.columnMetaData = column.getMeta_data();
+
+    // TODO: Encryption support
+
+    if (column.isSetCrypto_metadata()) { // column metadata is encrypted
+
+      ColumnCryptoMetaData ccmd = column.getCrypto_metadata();
+
+      if (ccmd.isSetENCRYPTION_WITH_COLUMN_KEY()) {
+
+        // TODO: CPP impl is in metadata.cc, line 186
+
+        throw new ParquetException(
+            "Cannot decrypt ColumnMetadata." +
+            " FileDecryption is not setup correctly");
+      }
+    }
+
+    this.encodings = new ArrayList<>();
+    this.encodingStats = new ArrayList<>();
+
+    for (org.apache.parquet.format.Encoding encoding : columnMetaData.getEncodings()) {
+      encodings.add(convertEnum(encoding, Encoding.class));
+    }
+
+    for (org.apache.parquet.format.PageEncodingStats encodingStats : columnMetaData.getEncoding_stats()) {
+      this.encodingStats.add(new PageEncodingStats(
+          convertEnum(encodingStats.getPage_type(), PageType.class),
+          convertEnum(encodingStats.getEncoding(), Encoding.class),
+          encodingStats.getCount()));
+    }
+
+    possibleStats = null;
+  }
+
+  private <T extends Enum<T>, S extends Enum<S>> T convertEnum(S source, Class<T> target) {
+    return Enum.valueOf(target, source.name());
+  }
+
+  // column chunk
+  public long fileOffset() {
+    return column.getFile_offset();
+  }
+
+  // parameter is only used when a dataset is spread across multiple files
+  public String filePath() {
+    return column.getFile_path();
+  }
+
+  // column metadata
+  public boolean isMetadataSet() {
+    return column.isSetMeta_data();
+  }
+
+  public ParquetType type() {
+    return convertEnum(columnMetaData.getType(), ParquetType.class);
+  }
+
+  public long numValues() {
+    return columnMetaData.getNum_values();
+  }
+
+  public ColumnPath pathInSchema() {
+    return new ColumnPath(columnMetaData.getPath_in_schema());
+  }
+
+  public List<Encoding> encodings() {
+    return encodings;
+  }
+
+  public List<PageEncodingStats> encodingStats() {
+    return encodingStats;
+  }
+
+  public boolean hasDictionaryPage() {
+    return columnMetaData.isSetDictionary_page_offset();
+  }
+
+  public long dictionary_page_offset() {
+    return columnMetaData.getDictionary_page_offset();
+  }
+
+  public long data_page_offset() {
+    return columnMetaData.getData_page_offset();
+  }
+
+  public boolean has_index_page() {
+    return columnMetaData.isSetIndex_page_offset();
+  }
+
+  public long index_page_offset() {
+    return columnMetaData.getIndex_page_offset();
+  }
+
+  public long total_compressed_size() {
+    return columnMetaData.getTotal_compressed_size();
+  }
+
+  public long total_uncompressed_size() {
+    return columnMetaData.getTotal_uncompressed_size();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    // Based on CPP ColumnChunkMetaDataImpl which only compares columnMetadata
+
+    if (this == o) { return true; }
+    if (!(o instanceof ColumnChunkMetadata)) { return false; }
+    ColumnChunkMetadata that = (ColumnChunkMetadata) o;
+    return Objects.equals(columnMetaData, that.columnMetaData);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(columnMetaData);
+  }
+
+  /** Check if statistics are set and are valid. */
+  public boolean isStatsSet() {
+
+    // Check if statistics are set and are valid
+    // 1) Must be set in the metadata
+    // 2) Statistics must not be corrupted
+
+    if (writerVersion == null) {
+      throw new ParquetException("Check failed: \"writerVersion != null\"");
+    }
+
+    // If the column statistics don't exist or column sort order is unknown
+    // we cannot use the column stats
+    if (!columnMetaData.isSetStatistics() || descriptor.sortOrder() == SortOrder.UNKNOWN) {
+      return false;
+    }
+
+    if (possibleStats == null) {
+      possibleStats = Statistics.makeColumnStats(columnMetaData, descriptor);
+    }
+
+    EncodedStatistics encodedStatistics = possibleStats.encode();
+
+    return writerVersion.hasCorrectStatistics(type(), encodedStatistics, descriptor.sortOrder());
+  }
+
+  public Statistics statistics() {
+    return isStatsSet() ? possibleStats : null;
+  }
+
+  // TODO: Compression
+  // Compression::type compression() const;
+  // Indicate if the ColumnChunk compression is supported by the current
+  // compiled parquet library.
+  // bool can_decompress() const;
+
+  // TODO: Crypto support
+  // ColumnCryptoMetaData cryptoMetadata() {}
+}
---
+++ java/adapter/parquet/src/main/java/org/apache/arrow/adapter/parquet/Statistics.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.arrow.adapter.parquet;
+
+import org.apache.parquet.format.ColumnMetaData;
+
+
+/** Base type for computing column statistics while writing a file. */
+public abstract class Statistics {
+
+  // TODO: Statistics
+
+  /** Make a typed statistics object depending on the type of the column. */
+  public static Statistics makeColumnStats(ColumnMetaData metadata, ColumnDescriptor descr) {
+    
+    // TODO: CPP impl is in metadata.cc, line 101
+
+    throw new ParquetException("Can't decode page statistics for selected column type");
+  }
+
+  public EncodedStatistics encode() {
+    return null;
+  }
+}
